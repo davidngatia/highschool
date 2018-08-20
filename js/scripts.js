28d1ff10e24daeeeb58b0e4d5d14514590e517f8
davidngatia <machngatia@gmail.com>
2018-08-20 09:20:43 +0300

    modify clickable

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,37 @@
-function onSubmit(e){
-  Logger.log("authMode=%s, source.getId()=%s", e.authMode, e.source.getId());
-  var items = e.response.getItemResponses();
-  for (i in items){
-    Logger.log("getItem().getTitle()=%s, getResponse()=%s", items[i].getItem().getTitle(), items[i].getResponse());
-  }
+function check(){
+var question1= document.quiz.question1.value;
+var question2= document.quiz.question2.value;
+var question3= document.quiz.question3.value;
+var correct=0;
+
+if (question1 === "JAVASCRIPT") {
+correct++;
+}
+
+if (question2 === "ECMAscript") {
+correct++;
+}
+
+if (question3 === "1995") {
+correct++;
+}
+
+var messages = ["Exellent!", "Good trial", "Try again!"];
+
+var range;
+
+if (correct < 1)
+{
+range = 2;
+}
+if (correct > 0 && correct < 3){
+range = 1;
+}
+if (correct > 2){
+range = 0;
+}
+
+document.getElementById("message").innerHTML= messages[range];
+document.getElementById("number_correct").innerHTML = "Your score is: " + correct + " correct ";
+document.getElementById("after_submit").style.visibility = "Visible";
+}
